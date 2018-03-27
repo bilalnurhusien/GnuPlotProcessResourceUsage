071a50f3421d7690b401125435a01cf6f555a7c4
Bilal Nurhusien <bilal.nurhusien@t2systems.com>
2018-03-27 14:50:25 -0700

    Trim resource usage files

--- psmon.sh
+++ psmon.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 
 process_list_path=/tmp/processlist.in
+max_file_length=2000
+trim_length=100
 
-if [ ! -f ${process_list_path} ]
+if [ ! -f ${process_list_path} ];
 	then
-	echo "Process list file doesn't exist"
+		echo "Process list file doesn't exist"
 	exit
 fi
 
 while (true) do
 	while read p; do
 		exist=`pgrep -f $p | wc -l`
-		if [ $exist -gt 0 ];
+		if [ "$exist" -gt 0 ]
 			then
+				file_size=`cat /tmp/${p}_resource_usage.dat | wc -l`
+				if [ "$file_size" -gt $max_file_length ]
+					then
+						sed -e '1,${trim_length}d' < /tmp/${p}_resource_usage.dat > /tmp/tmp_${p}_resource_usage.dat
+						mv /tmp/tmp_${p}_resource_usage.dat /tmp/${p}_resource_usage.dat
+				fi
+
 				ps -p `pgrep -f $p` -o %cpu,%mem | awk -v timestamp=$(date +"%H:%M") 'NR==2{print timestamp,$1,$2}' >> /tmp/${p}_resource_usage.dat
 		fi
 	done < ${process_list_path}
 	sleep 60
 done
+
 
 
