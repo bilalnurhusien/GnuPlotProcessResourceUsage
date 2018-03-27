959cb1a8eecd0981d5bdb05f0b474fd0f4496fd0
Bilal Nurhusien <bilal.nurhusien@t2systems.com>
2018-03-27 12:33:58 -0700

    Add pgrep -f full pattern matching

--- psmon.sh
+++ psmon.sh
@@ -2,12 +2,18 @@
 
 process_list_path=/tmp/processlist.in
 
+if [ ! -f ${process_list_path} ]
+	then
+	echo "Process list file doesn't exist"
+	exit
+fi
+
 while (true) do
 	while read p; do
-		exist=`pgrep $p | wc -l`
+		exist=`pgrep -f $p | wc -l`
 		if [ $exist -gt 0 ];
 			then
-				ps -p `pgrep $p` -o %cpu,%mem | awk -v timestamp=$(date +"%H:%M") 'NR==2{print timestamp,$1,$2}' >> /tmp/${p}_resource_usage.dat
+				ps -p `pgrep -f $p` -o %cpu,%mem | awk -v timestamp=$(date +"%H:%M") 'NR==2{print timestamp,$1,$2}' >> /tmp/${p}_resource_usage.dat
 		fi
 	done < ${process_list_path}
 	sleep 60
